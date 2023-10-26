a07cbb2ecd72736266b54947e0421eac6d51d812
Haf <haf@protonmail.ch>
2023-10-26 12:25:14 +0200

    no longer mixin departures from ALGECEUT with CEUTALGE

--- backend/Controller/ferryController.js
+++ backend/Controller/ferryController.js
@@ -10,15 +10,12 @@ router.get("/departures", async (req, res) => {
     const today = new Date();
     const twoMonthsLater = new Date(today.setMonth(today.getMonth() + 2));
     const ALGECIRAS = "ALGECEUT";
-    const CEUTA = "CEUTALGE";
     const urlALG = `https://tadpole.clickferry.app/departures?route=${ALGECIRAS}&time=`;
-    const urlCEU = `https://tadpole.clickferry.app/departures?route=${CEUTA}&time=`;
     const dates = {};
     for (let d = new Date(); d <= twoMonthsLater; d.setDate(d.getDate() + 1)) {
       const date = d.toISOString().split("T")[0];
       const response1 = await axios.get(urlALG + date);
-      const response2 = await axios.get(urlCEU + date);
-      if (response1.data.length > 0 || response2.data.length > 0) {
+      if (response1.data.length > 0) {
         dates[date] = true;
       }
     }
